6f7ee746c2d1b414240519ab1a6535fff335b907
dakinet <134066259+dakinet@users.noreply.github.com>
2025-04-01 08:58:06 +0200

    Update flags-handler.js

--- assets/js/flags-handler.js
+++ assets/js/flags-handler.js
@@ -22,25 +22,25 @@ const FlagHandler = {
     },
     
     // Update flag grid to show flags starting with specific letter
-    function updateFlagGrid() {
+   updateFlagGrid: function() {
     const flagGrid = document.getElementById('flagGrid');
     if (!flagGrid) return;
     
     // Clear grid
     flagGrid.innerHTML = '';
-    
+
     // Add all flags to grid (no filtering by letter)
     Object.values(this.flags).forEach(flag => {
         const flagImg = document.createElement('img');
-        flagImg.src = `assets/flags/${flag.code.toLowerCase()}.gif`; 
+        flagImg.src = `assets/flags/${flag.code.toLowerCase()}.gif`;
         flagImg.alt = flag.name;
         flagImg.title = flag.name;
         flagImg.className = 'flag-item';
         flagImg.dataset.code = flag.code;
-        
+
         flagGrid.appendChild(flagImg);
     });
-}
+},
     
     // Select a flag
     selectFlag: function(flagElement) {
